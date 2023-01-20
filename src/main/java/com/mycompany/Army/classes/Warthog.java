e5e85bd172761ba206c9d74a29214fe727caefc7
andrewgobert <108718840+andrewgobert@users.noreply.github.com>
2023-01-20 02:12:05 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/Army/classes/Warthog.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Army.Classes;
+
+import com.mycompany.Army.Enums.Flight;
+
+/**
+ *
+ * @author Andrew Gobert
+ */
+public class Warthog extends Plane {
+    private String[] weapon = new String[3];
+    private Flight[] position = new Flight[2];
+    
+    public void setWeapon(String weapon, int n){
+        this.weapon[n] = weapon;
+    }
+    public String getWeapon(int n){
+        return this.weapon[n];
+    }
+    public void setPosition(Flight Position, int n){
+        this.position[n] = Position;
+    }
+    public Flight getPosition(int n){
+        return position[n];
+    }
+}
